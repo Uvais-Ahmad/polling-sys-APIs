15c52019e5c87cf8878227ff6a7933964ef17c5a
Uvais Ahmad <uvss21@gmail.com>
2022-11-23 22:12:42 +0530

    Models created of Ques And Options

--- controller/api/questions_Controller.js
+++ controller/api/questions_Controller.js
@@ -6,3 +6,18 @@ module.exports.index = function( req , res ){
         }
     })
 }
+
+
+module.exports.createQuestion = function ( req , res ) {
+
+    
+
+    let ques = req.body;
+    console.log("Req body : ",ques)
+    return res.status(200).json({
+        message : 'form Data received',
+        data : {
+            question : ques
+        }
+    })
+}
