9f158095a641a7dcf29e1e661cc0c0b13ccc109d
Uvais Ahmad <uvss21@gmail.com>
2022-11-29 23:01:16 +0530

    update

--- router/api/v1/index.js
+++ router/api/v1/index.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',function( req , res){
-    return res.status(200).json({
-        message : "APIs url for performing operation",
-        data : {
-            create_Ques : "/api/v1/questions/create",
-            show_Ques : "/api/v1/options/:id",
-            delete_Ques : "/api/v1/options/:id/delete",
-            create_Opts : "/api/v1/options/:id/create",
-            add_Vote : "/api/v1/options/:id/add-vote",
-            delete_Opts : "/api/v1/options/:id/delete"
-        }
-    })
-})
-
 // this router file handle all routers in v1 file
 router.use('/questions',require('./questions'));
 
--- router/index.js
+++ router/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 // this router file use to handle all file incl /api
 
+router.get('/',function( req , res){
+    return res.status(200).json({
+        message : "APIs url for performing operation",
+        data : {
+            create_Ques : "/api/v1/questions/create",
+            show_Ques : "/api/v1/options/:id",
+            delete_Ques : "/api/v1/options/:id/delete",
+            create_Opts : "/api/v1/options/:id/create",
+            add_Vote : "/api/v1/options/:id/add-vote",
+            delete_Opts : "/api/v1/options/:id/delete"
+        }
+    })
+})
+
 router.use('/api',require('./api'));
 
 module.exports = router;
